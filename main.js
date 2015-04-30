82e818d716ebe3fa4ed21c293cf4fa6e3e94c42b
earthspace <earthspace@ml1.net>
2015-04-30 10:58:12 +0100

    laziness is the mother of invention

---
+++ main.js
@@ -0,0 +1,95 @@
+define(function (require, exports, module) {
+	"use strict";
+
+	var CommandManager      = brackets.getModule("command/CommandManager"),
+		FileSystem          = brackets.getModule('filesystem/FileSystem'),
+		DocumentManager     = brackets.getModule('document/DocumentManager'),
+		EditorManager       = brackets.getModule('editor/EditorManager'),
+		Widgets             = brackets.getModule('widgets/Dialogs'),
+		Menus               = brackets.getModule("command/Menus"),
+		AppInit             = brackets.getModule("utils/AppInit"),
+		ExtensionUtils      = brackets.getModule('utils/ExtensionUtils'),
+		NodeConnection      = brackets.getModule('utils/NodeConnection'),
+		node                = new NodeConnection(),
+		Strings             = require("strings"),
+		GFT_CMD_ID          = "gofmt.runfmt";
+
+	function endGoFmt() {
+		$('#gofmt-icon').addClass('easeOut');
+		$('#gofmt-icon').removeClass('on');
+	}
+
+	function initGoFmt() {
+		var icon = $("<a  href='#' id='gofmt-icon'> </a>");
+		icon.attr("title", Strings.FORMAT_THIS_FILE);
+		icon.on("click", handleIconClick);
+		icon.appendTo($("#main-toolbar .buttons"));
+		ExtensionUtils.loadStyleSheet(module, "styles/gofmt.css");
+	}
+
+	function startGoFmt() {
+		var editor = EditorManager.getFocusedEditor(),
+			currentDocument = DocumentManager.getCurrentDocument();
+
+		if (currentDocument.language._name === 'Golang') {
+			if (editor && editor.document) {
+				var cursorPos = editor.getCursorPos(),
+					fileBody = editor.document.getText();
+					
+				var tmpFilePath = currentDocument.file._path + '.tmp';
+				var tmpFile = FileSystem.getFileForPath(tmpFilePath);
+				
+				tmpFile.exists(function (err, exists) {
+					if (!exists) {
+						tmpFile.write(fileBody);
+						node.domains.gofmt.formatFile(tmpFilePath).done(function (data) {
+							if (!data.match(/gofmt: command not found/)) {
+								editor.selectAllNoScroll();
+								editor.document.setText(data);
+								editor.setCursorPos(cursorPos.line, cursorPos.ch, true);
+								endGoFmt();
+							} else {
+								Widgets.showModalDialog(
+									brackets.DIALOG_ID_SAVE_CLOSE,
+									Strings.MISSING_GOFMT_TITLE,
+									Strings.MISSING_GOFMT_BODY_1 +
+									Strings.MISSING_GOFMT_BODY_2 +
+									"<br><br><input style='width:100%' value='sudo ln -s /usr/local/go/bin/gofmt /usr/bin/gofmt'><br><br>" +
+									Strings.MISSING_GOFMT_BODY_3
+								)
+							}
+							tmpFile.unlink();
+						});
+					} else {
+						tmpFile.unlink();
+					}
+				});
+			}
+		}
+	}
+
+	function handleIconClick() {
+		$('#gofmt-icon').removeClass('easeOut');
+		if (!$('#gofmt-icon').hasClass('on')) {
+			$('#gofmt-icon').addClass('on');
+		}
+		startGoFmt();
+	}
+
+	if (!node.domains.gofmt) {
+		node.connect(true).done(function () {
+			var path = ExtensionUtils.getModulePath(module, 'node/gofmt.js');
+			node.loadDomains([path], true).done(function () {
+				AppInit.appReady(initGoFmt);
+			});
+		});
+	} else {
+		AppInit.appReady(initGoFmt);
+	}
+
+	CommandManager.register(Strings.FORMAT_THIS_FILE, GFT_CMD_ID, handleIconClick);
+
+	var menu = Menus.getMenu(Menus.AppMenuBar.EDIT_MENU);
+	menu.addMenuItem(GFT_CMD_ID);
+
+});
---
+++ nls/nl/strings.js
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2012 Adobe Systems Incorporated. All rights reserved.
+ *  
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"), 
+ * to deal in the Software without restriction, including without limitation 
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense, 
+ * and/or sell copies of the Software, and to permit persons to whom the 
+ * Software is furnished to do so, subject to the following conditions:
+ *  
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *  
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
+ * DEALINGS IN THE SOFTWARE.
+ * 
+ */
+
+// Dutch translation
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50 */
+/*global define */
+
+define({
+    "FORMAT_THIS_FILE": "Format met gofmt",
+    "MISSING_GOFMT_TITLE": "Kan niet vinden gofmt uitvoerbaar",
+    "MISSING_GOFMT_BODY_1": "De executable voor gofmt is niet gevonden op de weg.",
+    "MISSING_GOFMT_BODY_2": "De gemakkelijkste manier om dit op te lossen is om een symbolische koppeling naar de gofmt executable, bijvoorbeeld te maken:",
+    "MISSING_GOFMT_BODY_3": "waarbij de eerste parameter is het resultaat van 'waarvan gofmt'."
+});
