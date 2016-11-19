88dd87312b0beb7bc6838ad85612ca1f88447ee9
Boris Kubiak <kubiakboris@gmail.com>
2016-11-19 18:20:29 +0100

    Proofread french and spanish translations.

--- nls/fr/strings.js
+++ nls/fr/strings.js
@@ -4,10 +4,10 @@
 /*global define*/
 
 define({
-    "FORMAT_THIS_FILE": "Format avec gofmt",
-    "MISSING_GOFMT_TITLE": "Impossible de trouver gofmt exécutable",
-    "MISSING_GOFMT_BODY_1": "L'exécutable pour gofmt n'a pas été trouvé sur le chemin.",
-    "MISSING_GOFMT_BODY_2": "La meilleure façon de résoudre ce problème est de créer un lien symbolique vers l'exécutable de gofmt, par exemple:",
-    "MISSING_GOFMT_BODY_3": "où le premier paramètre est le résultat de 'qui gofmt'.",
-    "MISSING_GOFMT_ERROR": "Le formatage du code erreur"
+    "FORMAT_THIS_FILE": "Formater avec gofmt",
+    "MISSING_GOFMT_TITLE": "Impossible de trouver l'exécutable gofmt ",
+    "MISSING_GOFMT_BODY_1": "L'exécutable pour gofmt n'a pas été trouvé.",
+    "MISSING_GOFMT_BODY_2": "La meilleure façon de résoudre ce problème est de créer un lien symbolique vers l'exécutable de gofmt, par exemple :",
+    "MISSING_GOFMT_BODY_3": "donde el primer parámetro es el resultado de 'which gofmt'.",
+    "MISSING_GOFMT_ERROR": "Erreur de formatage du code"
 });
