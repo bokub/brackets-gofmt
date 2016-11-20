f209c2cd432cfd3db950b6cd57aef6bfb8d482b8
Boris Kubiak <kubiakboris@gmail.com>
2016-11-20 16:44:54 +0100

    Error handling, better error messages, lots of bug fixes,  jslint passing, added more comments

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/*jslint vars: true, nomen: true, devel: false*/
+/*global define, brackets, $*/
+
 define(function (require, exports, module) {
     "use strict";
 
@@ -10,71 +13,87 @@ define(function (require, exports, module) {
         AppInit = brackets.getModule("utils/AppInit"),
         ExtensionUtils = brackets.getModule('utils/ExtensionUtils'),
         NodeConnection = brackets.getModule('utils/NodeConnection'),
+        ThemeManager = brackets.getModule('view/ThemeManager'),
         node = new NodeConnection(),
         Strings = require("strings"),
         GFT_CMD_ID = "gofmt.runfmt";
 
+    /** Sets the icon to its original state */
     function endGoFmt() {
         var icon = $('#gofmt-icon');
         icon.addClass('easeOut');
         icon.removeClass('on');
     }
 
-    function initGoFmt() {
-        var icon = $("<a  href='#' id='gofmt-icon'> </a>");
-        icon.attr("title", Strings.FORMAT_THIS_FILE);
-        icon.on("click", handleIconClick);
-        icon.appendTo($("#main-toolbar").find(".buttons"));
-        ExtensionUtils.loadStyleSheet(module, "styles/gofmt.css");
+    /** Shows dialg with an error message */
+    function showErrorDialog(errorMessage, args) {
+        if (typeof args !== 'undefined') {
+            var key;
+            for (key in args) {
+                if (args.hasOwnProperty(key)) {
+                    errorMessage = errorMessage.replace("_" + key + "_", args[key]);
+                }
+            }
+        }
+        Widgets.showModalDialog(brackets.DIALOG_ID_SAVE_CLOSE, Strings.ERROR_TITLE, errorMessage);
+        endGoFmt();
     }
 
+    /** Adds colors and <br> tags to a gofmt error message */
+    function formatGoErrors(message) {
+        var colors = ThemeManager.getCurrentTheme().dark ? ['#c8c8c8', '#6bbeff', '#ff9d2a'] : ['#333333', '#0083e8', '#e27100'];
+        return ('<span style="color:' + colors[0] + '">') +
+            message.replace(/(\S+)\.tmp\:(\d*?\:\d*?)\:/g, '<span style="color:' + colors[1] + '">$1</span><span style="color:' + colors[2] + ';font-weight:bold">&nbsp;$2&nbsp; </span>')
+            .replace(/\n/g, '<br>') + '</span>';
+    }
+
+    /** The main function, called when clicking the button or pressing the shortcut */
     function startGoFmt() {
-        var editor = EditorManager.getFocusedEditor(),
+        var editor = EditorManager.getFocusedEditor() || EditorManager.getActiveEditor(),
             currentDocument = DocumentManager.getCurrentDocument();
+        if (currentDocument === null) {
+            showErrorDialog(Strings.ERROR_NO_CURRENT_FILE);
+            return;
+        }
+        if (currentDocument.language._name !== 'Golang') {
+            showErrorDialog(Strings.ERROR_NOT_GO_FILE, {language: currentDocument.language._name});
+            return;
+        }
 
-        if (currentDocument.language._name === 'Golang') {
-            if (editor && editor.document) {
-                var cursorPos = editor.getCursorPos(),
-                    fileBody = editor.document.getText();
+        if (!editor.document) {
+            endGoFmt();
+            return;
+        }
 
-                var tmpFilePath = currentDocument.file._path + '.tmp';
-                var tmpFile = FileSystem.getFileForPath(tmpFilePath);
+        var cursorPos = editor.getCursorPos(),
+            fileBody = editor.document.getText();
 
-                tmpFile.exists(function (err, exists) {
-                    if (!exists) {
-                        tmpFile.write(fileBody);
-                        node.domains.gofmt.formatFile(tmpFilePath).done(function (data) {
-                            if (data.match(/gofmt: command not found/)) {
-                                Widgets.showModalDialog(
-                                    brackets.DIALOG_ID_SAVE_CLOSE,
-                                    Strings.MISSING_GOFMT_TITLE,
-                                    Strings.MISSING_GOFMT_BODY_1 +
-                                    Strings.MISSING_GOFMT_BODY_2 +
-                                    "<br><br><input style='width:100%' value='sudo ln -s /usr/local/go/bin/gofmt /usr/bin/gofmt'><br><br>" +
-                                    Strings.MISSING_GOFMT_BODY_3
-                                );
-                            } else if (data.match(/\.tmp\:\d*?\:\d*?\:/)) {
-                                Widgets.showModalDialog(
-                                    brackets.DIALOG_ID_SAVE_CLOSE,
-                                    Strings.MISSING_GOFMT_ERROR,
-                                    data
-                                );
-                            } else {
-                                editor.selectAllNoScroll();
-                                editor.document.setText(data);
-                                editor.setCursorPos(cursorPos.line, cursorPos.ch, true);
-                                endGoFmt();
-                            }
-                            tmpFile.unlink();
-                        });
+        var tmpFilePath = currentDocument.file._path + '.tmp';
+        var tmpFile = FileSystem.getFileForPath(tmpFilePath);
+
+        tmpFile.exists(function (err, exists) {
+            if (!exists) {
+                tmpFile.write(fileBody);
+                node.domains.gofmt.formatFile(tmpFilePath).done(function (data) {
+                    var index = data.indexOf('gofmt');
+                    if (index === 0 || index === 1) {
+                        showErrorDialog(data);
+                    } else if (data.match(/\.tmp\:\d*?\:\d*?\:/)) {
+                        showErrorDialog(formatGoErrors(data));
                     } else {
-                        tmpFile.unlink();
+                        editor.selectAllNoScroll();
+                        editor.document.setText(data);
+                        editor.setCursorPos(cursorPos.line, cursorPos.ch, true);
+                        endGoFmt();
                     }
                 });
             }
-        }
+
+            tmpFile.unlink();
+        });
     }
 
+    /** Styles the gofmt icon */
     function handleIconClick() {
         var icon = $('#gofmt-icon');
         icon.removeClass('easeOut');
@@ -82,6 +101,15 @@ define(function (require, exports, module) {
             icon.addClass('on');
         }
         startGoFmt();
+    }
+
+    /** Adds the icon in the toolbar */
+    function initGoFmt() {
+        var icon = $("<a  href='#' id='gofmt-icon'> </a>");
+        icon.attr("title", Strings.FORMAT_THIS_FILE);
+        icon.on("click", handleIconClick);
+        icon.appendTo($("#main-toolbar").find(".buttons"));
+        ExtensionUtils.loadStyleSheet(module, "styles/gofmt.css");
     }
 
     if (!node.domains.gofmt) {
