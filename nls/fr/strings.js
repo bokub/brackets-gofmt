3f9b8483eade552d8ade12fad362d530b430de5c
Boris Kubiak <kubiakboris@gmail.com>
2016-12-15 12:28:04 +0100

    French translation

--- nls/fr/strings.js
+++ nls/fr/strings.js
@@ -4,8 +4,27 @@
 /*global define*/
 
 define({
-    "FORMAT_THIS_FILE": "Formater avec gofmt",
-    "ERROR_TITLE": "Erreur gofmt",
-    "ERROR_NO_CURRENT_FILE": "Aucun fichier n'est actuellement ouvert.<br>Ouvrez un fichier go et essayez à nouveau.",
-    "ERROR_NOT_GO_FILE": "Le fichier actuel est un fichier _language_.<br>Gofmt peut formater uniquement les fichiers Go."
+    FORMAT_THIS_FILE: "Formater avec Go Formatter",
+    ERROR_TITLE: "Erreur gofmt",
+    ERROR_NO_CURRENT_FILE: "Aucun fichier n'est actuellement ouvert.<br>Ouvrez un fichier go et essayez à nouveau.",
+    ERROR_NOT_GO_FILE: "Le fichier actuel est un fichier _language_.<br>Gofmt peut formater uniquement les fichiers Go.",
+
+    SETTINGS_CMD: "Paramètres de Go Formatter",
+    SETTINGS_SHORTCUT: "Raccourci Go Formatter",
+    SETTINGS_SHORTCUTS_HINT: "Touches séparées par un tiret.<br>Comme ceci : Ctrl-Alt-F",
+    SETTINGS_GO_IMPORTS: "Lancer goimports après formatage du code",
+    SETTINGS_GOPATH: "Go path",
+    SETTINGS_GOPATH_HINT: "Tapez <span class=\"code\">go env GOPATH</span> dans un terminal pour obtenir le GOPATH",
+    SETTINGS_GOFMT_PATH: "Emplacement de gofmt",
+    SETTINGS_GOFMT_PATH_HINT: "Tapez <span class=\"code\">which gofmt</span> dans un terminal pour obtenir l'emplacement",
+    SETTINGS_GOIMPORTS_PATH: "Emplacement de  goimports",
+    SETTINGS_GOIMPORTS_PATH_HINT: "Tapez <span class=\"code\">which goimports</span> dans un terminal pour obtenir l'emplacement",
+
+    Q_RESTART_BRACKETS: "Souhaitez-vous redémarrer Brackets pour appliquer les nouveaux paramètres ?",
+    RESTART: "Redémarrer",
+
+    BTN_OK: "OK",
+    BTN_SAVE: "Enregistrer",
+    BTN_CANCEL: "Annuler",
+    BTN_DEFAULT: "Paramètres par défaut"
 });
